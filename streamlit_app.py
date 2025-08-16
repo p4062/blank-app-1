fb4ff17217277076fad5ac5cc70714ebc251fa6a
p4062 <p4062@tarc.edu.my>
2025-08-16 08:18:11 +0000

    version1

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ user_input = st.number_input('Enter house size:', min_value=100, max_value=10000
 
 # Reshape the input for the model
 input_array = np.array([user_input]).reshape(-1, 1)
-
+st.write(f"Testing") 
 # Predict the house price
 if st.button('Predict Price'):
     predicted_price = model.predict(input_array)
